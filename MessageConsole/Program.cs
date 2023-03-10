93d2d4472c446ed073630daf318af98ec2d0c10e
Standa Chvila <schvila@gmail.com>
2023-03-10 17:04:36 +0100

    Add Viber

--- MessageConsole/Program.cs
+++ MessageConsole/Program.cs
@@ -3,19 +3,31 @@
 
 using System.Text;
 using WhatsApp;
+using Viber;
 
-var security = "NjEyNzhjYmRiODFjOWViN2E2YTA4NGYyNDZmODBmZTk=";
-byte[] b = Convert.FromBase64String(security);
-string accountSID = Decrypt("QUNlOTkxY2VhNDdjMDkwNWMxZjc1NDZiMDFhZDU3OTU4ZQ==");
-var liborMobil = "+420607046754";
-var token = Decrypt("NjEyNzhjYmRiODFjOWViN2E2YTA4NGYyNDZmODBmZTk=");
-var twilioSendBoxNr = "+14155238886";
-var lib = new WhatsAppLib();
-lib.Init(accountSID, token);
-lib.SendMessage("+14155238886", liborMobil, "WhatsApp api test using Twilio");
+//WhatsApp();
+await ViberTest();
 string Decrypt(string pwd)
 {
     byte[] b = Convert.FromBase64String(pwd);
     return Encoding.UTF8.GetString(b);
+
+}
+
+ async Task ViberTest()
+{
+    var viber = new ViberLib();
+    var ret = await viber.SendMessageAsync("+420603446641", @"Send by Standa's Viber.Bot");
+}
+void WhatsAppTest()
+{
+    string accountSID = Decrypt("QUNlOTkxY2VhNDdjMDkwNWMxZjc1NDZiMDFhZDU3OTU4ZQ==");
+    var liborMobil = "+420607046754";
+    var token = Decrypt("NjEyNzhjYmRiODFjOWViN2E2YTA4NGYyNDZmODBmZTk=");
+    var twilioSendBoxNr = "+14155238886";
+    var lib = new WhatsAppLib();
+    lib.Init(accountSID, token);
+    //lib.SendMessage(twilioSendBoxNr, liborMobil, "WhatsApp api test using Twilio");
+    lib.SendMessage(liborMobil, twilioSendBoxNr, "WhatsApp api test using Twilio");
 
 }
--- Viber/ViberLib.cs
+++ Viber/ViberLib.cs
@@ -1,15 +1,49 @@
 ﻿using System.Runtime.CompilerServices;
+using System.Text;
 using Viber.Bot;
 
 namespace Viber;
 public class ViberLib
 {
-    //private _bot = new ViberBot("<auth_token>");
-
-    public async void SendMessageAsync(string recieverId, string message)
+    private ViberBotClient _bot;// = new ViberBot("<auth_token>");
+    public ViberLib()
     {
-        var bot = new ViberBotClient("<auth_token>");
-        return await bot.SendTextMessageAsync(recieverId, message);
+        byte[] b = Convert.FromBase64String("NTBiMjg1MTU5ODI3ZTE5YS02NTljMTIyNjg0OTEzMjA0LTY5OWZkM2IwMTdkZTZhYjE=");
+        var token = Encoding.UTF8.GetString(b);
+        _bot = new ViberBotClient(token);
+
+    }
+    public async Task<long> SendMessageAsync(string receiver, string text)
+    {
+        var message = new BroadcastMessage
+        {
+            Text = text,
+            BroadcastList = new List<string> { receiver }
+        };
+        return await _bot.SendBroadcastMessageAsync(message);
+    }
+    public async Task<long> SendMessagesAsync(List<string> receivers, string text)
+    {
+        var message = new BroadcastMessage
+        {
+            Text = text,
+            BroadcastList = receivers
+        };
+        return await _bot.SendBroadcastMessageAsync(message);
     }
 
 }
+/*
+var botClient = new ViberBotClient("YOUR_AUTH_TOKEN_HERE");
+var receiver = new Receiver
+{
+    Id = "01234567890A=",
+};
+var message = new TextMessage
+{
+    Text = "Hello, world!",
+};
+var receivers = new List<Receiver> { receiver };
+await1 botClient.SendBroadcastMessageAsync(message, receivers);
+
+ */
