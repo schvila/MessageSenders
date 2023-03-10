2d8ea4ae568341019ac2011f8b03322118bcf701
Standa Chvila <schvila@gmail.com>
2023-03-10 09:17:52 +0100

    Encryption like

--- MessageConsole/Program.cs
+++ MessageConsole/Program.cs
@@ -1,35 +1,21 @@
 ﻿// Install the C# / .NET helper library from twilio.com/docs/csharp/install
 
 
+using System.Text;
 using WhatsApp;
 
-
-var token = "afab8efa3d285871beb4ba5b461c266f";
+var security = "NjEyNzhjYmRiODFjOWViN2E2YTA4NGYyNDZmODBmZTk=";
+byte[] b = Convert.FromBase64String(security);
+string accountSID = Decrypt("QUNlOTkxY2VhNDdjMDkwNWMxZjc1NDZiMDFhZDU3OTU4ZQ==");
+var liborMobil = "+420607046754";
+var token = Decrypt("NjEyNzhjYmRiODFjOWViN2E2YTA4NGYyNDZmODBmZTk=");
 var twilioSendBoxNr = "+14155238886";
 var lib = new WhatsAppLib();
 lib.Init(accountSID, token);
-//lib.SendMessage("+14155238886", "+420603446641", "WhatsApp api test using Twilio");
-lib.SendMessage(twilioSendBoxNr, "+420607046754", "WhatsApp api test using Twilio");
+lib.SendMessage("+14155238886", liborMobil, "WhatsApp api test using Twilio");
+string Decrypt(string pwd)
+{
+    byte[] b = Convert.FromBase64String(pwd);
+    return Encoding.UTF8.GetString(b);
 
-// using Twilio;
-// using Twilio.Rest.Api.V2010.Account;
-// using Twilio.Types;
-//
-// class Example
-// {
-//     static void Main(string[] args)
-//     {
-//         var accountSid = "ACe991cea47c0905c1f7546b01ad57958e";
-//         var authToken = "afab8efa3d285871beb4ba5b461c266f";
-//         TwilioClient.Init(accountSid, authToken);
-//
-//         var messageOptions = new CreateMessageOptions(
-//           new PhoneNumber("whatsapp:+420603446641"));
-//         messageOptions.From = new PhoneNumber("whatsapp:+14155238886");
-//         messageOptions.Body = "Your appointment is coming up on July 21 at 3PM";
-//
-//
-//         var message = MessageResource.Create(messageOptions);
-//         Console.WriteLine(message.Body);
-//     }
-// }
+}
